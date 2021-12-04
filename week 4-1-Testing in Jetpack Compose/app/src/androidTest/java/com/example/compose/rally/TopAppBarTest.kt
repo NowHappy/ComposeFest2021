b121e159887149de9697be673ce181e298537d6b
joy <joy.y@kakaocorp.com>
2021-12-04 19:23:10 +0900

    week 4-1 Debugging tests

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,8 +3,12 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
+import java.util.Locale
 import org.junit.Rule
 import org.junit.Test
 
@@ -28,5 +32,25 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTheme{
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = { },
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+        }
+
+        // composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 }
